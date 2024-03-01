a450286ae8683d41ffa66f566658d66dc524851e
Vinicius Bastos Clemente <vclemente2@hotmail.com>
2024-03-01 18:05:08 -0300

    refac: otimizacao e padronizacao de nomenclatura dos testes.

--- src/test/java/temaController/TemaControllerTest.java
+++ src/test/java/temaController/TemaControllerTest.java
@@ -11,7 +11,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import util.AuthUtils;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
 
 public class TemaControllerTest extends TemaDataFactory {
 
@@ -24,49 +25,61 @@ public class TemaControllerTest extends TemaDataFactory {
     }
 
     @Test
-    @DisplayName("Listar temas com sucesso")
+    @DisplayName("CT-API-03.1 - Listar temas com sucesso")
     public void testListarTemas() {
 
         client.listar(0,10,token)
                 .then()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
         ;
     }
 
     @Test
-    @DisplayName("Listar temas com token inválido sem sucesso")
+    @DisplayName("CT-API-03.2 - Listar temas com token inválido sem sucesso")
     public void testListarTemasComTokenInvalido() {
 
         client.listar(0,10, "TOKEN_INVALIDO")
                 .then()
-                .statusCode(500)
+                .statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR)
         ;
     }
+
     @Test
-    @DisplayName("Adicionar tema com sucesso sem sucesso")
+    @DisplayName("CT-API-03.3 - Adicionar tema com sucesso sem sucesso")
     public void testAdicionarTemaComSucesso() {
 
         client.cadastrar(TemaDataFactory.gerarTemaValido(), token)
                 .then()
-                .statusCode(201);
+                .statusCode(HttpStatus.SC_CREATED);
     }
 
     @Test
-    @DisplayName("Adicionar tema com token inválido")
+    @DisplayName("CT-API-03.5 - Adicionar tema com token inválido")
     public void testAdicionarTemaComTokenInvalido() {
 
         client.cadastrar(TemaDataFactory.gerarTemaValido(), "TOKEN_INVALIDO")
                 .then()
-                .statusCode(500);
+                .statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
     }
 
     @Test
-    @DisplayName("Adicionar tema já cadastrado sem sucesso")
+    @DisplayName("CT-API-03.6 - Adicionar tema já cadastrado sem sucesso")
     public void testAdicionarTemaJaCadastrado() {
 
         client.cadastrar(new Tema("JAVA"), token)
                 .then()
-                .statusCode(400);
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    @DisplayName("CT-API-03.7 - Listar temas com informando quantidade de registros negativo sem sucesso")
+    public void testListarTemasComQuantidadeRegistroNegativa() {
+
+        client.listar(0,-1, token)
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("message", equalTo("findAll.quantidadeRegistros: must be greater than 0"))
+        ;
     }
 
     @ParameterizedTest
